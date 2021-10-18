81935b71897219e98203802e2e9e53e735f5eae6
anshu pasrija <anshu.pasrija@gmail.com>
2021-10-18 22:17:25 +0000

    0 failures

---
+++ .bundle/ruby/2.3.0/gems/nokogiri-1.10.10/ext/nokogiri/siteconf20211018-9991-jkmo21.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20211018-9991-1yxgpfo"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
